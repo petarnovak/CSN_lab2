5985e2aae415f68d83ceb421fe4c29ee8cd2c00d
GioSpi01 <163931674+GioSpi01@users.noreply.github.com>
2024-09-26 22:31:36 +0200

    Table
    Table problems solved, started point 2

---
+++ Rscript-novak-spisso.R
@@ -0,0 +1,32 @@
+#1: Introduction
+write_table <- function(language,file,lang.df) {
+   degree_sequence = read.table(file, header = FALSE)
+   lang.df <- rbind(lang.df,data.frame(language, length(degree_sequence$V1), max(degree_sequence$V1),
+                                       sum(degree_sequence$V1)/length(degree_sequence$V1),
+                                       length(degree_sequence$V1)/sum(degree_sequence$V1)))
+   return(lang.df)
+   }
+
+source = read.table("list_in.txt", 
+         header = TRUE,               # this is to indicate the first line of the file contains the names of the columns instead of the real data
+         as.is = c("language","file") # this is need to have the cells treated as real strings and not as categorial data.
+        )
+
+lang.df <- data.frame()
+
+for (x in 1:nrow(source)) {
+  lang.df <- write_table(source$language[x], source$file[x],lang.df)
+}
+colnames(lang.df) <- c("Language", "N", "Maximum degree", "M/N", "N/M")
+#Table with all languages and some important values
+lang.df
+
+#2: Visualization
+#Arabic
+
+#English
+degree_sequence = read.table("./data/English_in-degree_sequence.txt", header = FALSE)
+degree_spectrum = table(degree_sequence)
+x <- degree_spectrum
+x
+#
