dacc22231d68e2e26ecef37eb071c3e30b9170bb
GioSpi01 <163931674+GioSpi01@users.noreply.github.com>
2024-10-01 12:37:22 +0200

    Started with Altmann

--- Rscript-novak-spisso.R
+++ Rscript-novak-spisso.R
@@ -1,7 +1,7 @@
 # 1: Introduction
 write_table <- function(label,file,df) {
    degree_sequence = read.table(file, header = FALSE)
-   lang.df <- rbind(df,data.frame(label, length(degree_sequence$V1), max(degree_sequence$V1),
+   df <- rbind(df,data.frame(label, length(degree_sequence$V1), max(degree_sequence$V1),
                                        sum(degree_sequence$V1)/length(degree_sequence$V1),
                                        length(degree_sequence$V1)/sum(degree_sequence$V1)))
    return(df)
@@ -73,8 +73,8 @@ mle_calc <- function(p,lambda,x){
   mle_geo <- mle(minus_log_like_geo,
                  start = list(p = p),
                  method = "L-BFGS-B",
-                 lower = c(0.0000001),
-                 upper = c(0.9999999))
+                 lower = c(0.01),
+                 upper = c(0.99))
   geo_par <- c(attributes(summary(mle_geo))$coef[1],attributes(summary(mle_geo))$m2logL)
   mle_pois <- mle(minus_log_like_pois,
                   start = list(lambda = lambda),
@@ -155,16 +155,7 @@ files <- list.files(path = folder_path, full.names = TRUE)
 prob_list <- c("geo 0.05","geo 0.1","geo 0.2","geo 0.4","geo 0.8","zeta 1.5","zeta 2.5",
           "zeta 2","zeta 3.5","zeta 3")
 
-# Visualization
-for (i in 1:length(files)){
-  degree_sequence = read.table(files[i], header = FALSE)
-  degree_spectrum = table(degree_sequence)
-  barplot(degree_spectrum, main = paste("Distribution :",prob_list[i]), xlab = "degree", ylab = "number of vertices", log = "y")
-}
-
-#
-# WORK IN PROGRESS
-#
+# Introduction
 prob.df <- data.frame()
 
 for (x in 1:length(files)) {
@@ -174,97 +165,40 @@ for (x in 1:length(files)) {
 colnames(prob.df) <- c("Distribution", "N", "Maximum degree", "M/N", "N/M")
 prob.df
 
-mle_calc <- function(i,prob,file,param.df,AIC.df){
-  # Geometric distribution
-  minus_log_like_geo <- function(p){
-    -(sum(x)-length(x)) * log(1-p) - length(x) * log(p)
-  }
-  
-  # Poisson distribution
-  minus_log_like_pois <- function(lambda){
-    C <- 0
-    for (i in 1:length(x)) {
-      C = C + sum(log(2:x[i]))
-    }
-    - sum(x) * log(lambda) + length(x) * (lambda + log(1-exp(1)^(-lambda))) + C
-  }
-  
-  # Zeta distribution
-  minus_log_like_zeta <- function(gamma){
-    length(x) * log(zeta(gamma)) + gamma * sum(log(x))
-  }
-  
-  # Zeta (gamma=2) distribution
-  minus_log_like_zeta2 <- function(){
-    mle <- length(x) * log(pi^2/6) + 2 * sum(log(x))
-    return(mle)
-  }
-  
-  # Right-truncated zeta distribution
-  minus_log_like_zeta_trunc <- function(gamma){
-    length(x) * log(sum((1:max(x))^(-gamma))) + gamma * sum(log(x)) 
-  }
-  
-  x <- read.table(file, header = FALSE)$V1
-  mle_geo <- mle(minus_log_like_geo,
-                 start = list(p = prob.df$"N/M"[i]),
-                 method = "L-BFGS-B",
-                 lower = c(0.01), #problems with zeta: gamma = 1.5, with smaller values it crashes
-                 upper = c(0.99))
-  mle_pois <- mle(minus_log_like_pois,
-                  start = list(lambda = prob.df$"M/N"[i]),
-                  method = "L-BFGS-B",
-                  lower = c(1.0000001))
-  mle_zeta <- mle(minus_log_like_zeta,
-                  start = list(gamma = 2),
-                  method = "L-BFGS-B",
-                  lower = c(1.0000001))
-  mle_zeta_trunc <- mle(minus_log_like_zeta_trunc,
-                        start = list(gamma = 2),
-                        method = "L-BFGS-B",
-                        lower = c(1.0000001))
-  
-  # 4: Finding the best models
-  best_p_geo <- attributes(summary(mle_geo))$coef[1]
-  best_lambda_pois <- attributes(summary(mle_pois))$coef[1]
-  best_gamma_zeta <- attributes(summary(mle_zeta))$coef[1]
-  best_gamma_zeta_trunc <- attributes(summary(mle_zeta_trunc))$coef[1]
-  best_h_max_zeta_trunc <- max(x)
-  param.df <- rbind(param.df,data.frame(prob, best_lambda_pois, best_p_geo, 
-                                        best_gamma_zeta, best_gamma_zeta_trunc,
-                                        best_h_max_zeta_trunc))
-  
-  # 5: Best model selection
-  get_AIC <- function(m2logL,K,N){
-    m2logL + 2*K*N/(N-K-1) # AIC with a correction for sample size
-  }
-  
-  AIC_list <- c()
-  AIC_list[1] <- get_AIC(attributes(summary(mle_geo))$m2logL,1,length(x))
-  AIC_list[2] <- get_AIC(attributes(summary(mle_pois))$m2logL,1,length(x))
-  AIC_list[3] <- get_AIC(attributes(summary(mle_zeta))$m2logL,1,length(x))
-  mle_zeta2 <- 2*minus_log_like_zeta2()
-  AIC_list[4] <- get_AIC(mle_zeta2,0,length(x))
-  AIC_list[5] <- get_AIC(attributes(summary(mle_zeta_trunc))$m2logL,1,length(x))
-  
-  best_AIC <- min(AIC_list)
-  AIC_list <- AIC_list-best_AIC
-  
-  AIC.df <- rbind(AIC.df,data.frame(prob,t(AIC_list)))
-  return(list(param.df, AIC.df))
+# Visualization
+for (i in 1:length(files)){
+  degree_sequence = read.table(files[i], header = FALSE)
+  degree_spectrum = table(degree_sequence)
+  barplot(degree_spectrum, main = paste("Distribution :",prob_list[i]), xlab = "degree", ylab = "number of vertices", log = "y")
 }
 
-param.df <- data.frame() #table with most likely parameters
-AIC.df <- data.frame()
+param.sample.df <- data.frame() #table with best parameters
+AIC.sample.df <- data.frame() #AIC table
 
 for (i in 1:length(files)){
-  tables <- mle_calc(i, prob_list[i], files[i],param.df,AIC.df)
-  param.df <- tables[[1]]
-  AIC.df <- tables[[2]]
+  x <- read.table(files[i], header = FALSE)$V1
+  param.list <- mle_calc(prob.df$"N/M"[i],prob.df$"M/N"[i],x)
+  param.sample.df <- rbind(param.sample.df,data.frame(prob.df$Distribution[i], param.list[1,],max(x)))
+  AIC.sample.df <- AIC_calc(prob.df$Distribution[i],param.list[2,],AIC.sample.df)
 }
 
-colnames(param.df) <- c("Distribution", "lambda", "p", "gamma 1","gamma 2","k max")
-colnames(AIC.df) <- c("Distribution", "1", "2", "3", "4","5")
+colnames(param.sample.df) <- c("Distribution", "lambda", "p", "gamma 1","gamma 2","k max")
+colnames(AIC.sample.df) <- c("Distribution", "1", "2", "3", "4","5")
 
-param.df
-AIC.df
+param.sample.df
+AIC.sample.df
+
+# Additional work
+# Altmann distribution
+minus_log_like_altmann <- function(gamma,delta){
+  gamma * sum(log(x)) + delta * sum(x) + sum(log(sum(x^(-gamma)*exp(-delta*x))))
+}
+x <- read.table(source$file[1], header = FALSE)$V1
+mle_altmann <- mle(minus_log_like_altmann,
+               start = list(gamma = 2, delta = 0.01),
+               method = "L-BFGS-B",
+               lower = c(1.000001,0))
+
+mle_altmann
+attributes(summary(mle_altmann))$coef[1]
+attributes(summary(mle_altmann))$m2logL
