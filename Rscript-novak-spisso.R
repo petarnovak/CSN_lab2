b471d71535620897c5bd035527f6c7c7c5edf4fe
GioSpi01 <163931674+GioSpi01@users.noreply.github.com>
2024-10-04 14:52:06 +0200

    Code more understandable

--- Rscript-novak-spisso.R
+++ Rscript-novak-spisso.R
@@ -1,5 +1,6 @@
-# INTRODUCTION
 library(xtable)
+
+# Function to create the initial summary table
 write_table <- function(label,file,df) {
    degree_sequence = read.table(file, header = FALSE)
    df <- rbind(df,data.frame(label, length(degree_sequence$V1), max(degree_sequence$V1),
@@ -7,21 +8,6 @@ write_table <- function(label,file,df) {
                                        length(degree_sequence$V1)/sum(degree_sequence$V1)))
    return(df)
    }
-
-source = read.table("list_in.txt", 
-         header = TRUE,               # this is to indicate the first line of the file contains the names of the columns instead of the real data
-         as.is = c("language","file") # this is need to have the cells treated as real strings and not as categorial data.
-        )
-
-lang.df <- data.frame()
-
-for (x in 1:nrow(source)) {
-  lang.df <- write_table(source$language[x], source$file[x],lang.df)
-}
-colnames(lang.df) <- c("Language", "N", "Maximum degree", "M/N", "N/M")
-
-lang.df #Table with all languages and some important values
-print(xtable(lang.df), file = "Table 1.tex")
 
 # LOG-LIKELIHOOD
 require(stats4) # for MLE
@@ -91,16 +77,84 @@ get_AIC <- function(m2logL,K,N){
   m2logL + 2*K*N/(N-K-1) # AIC with a correction for sample size
 }
 
+# Function to create the AIC table and
+# to store the best AIC
 AIC_calc <- function(label, m2logL.list,df,x){
-  AIC.list <- sapply(m2logL.list,get_AIC,K=1,N=length(x))
+  AIC.list <- sapply(m2logL.list,get_AIC,K=1,N=length(x)) #row of the table
   mle_zeta2 <- 2*minus_log_like_zeta2()
   AIC.list <- append(AIC.list,get_AIC(mle_zeta2,0,length(x)),after = 3)
-  best.AIC <- min(AIC.list)
-  AIC.list <- AIC.list-best.AIC
+  best.AIC <- min(AIC.list) #best AIC
+  AIC.list <- AIC.list-best.AIC 
   df <- rbind(df,data.frame(label,t(AIC.list)))
   return(list(AICdf = df, AICbest = best.AIC))
 }
 
+# PLOTS
+# Probability functions, necessary to plot the distributions
+#Displaced geometric
+geo_dist <- function(p,k){
+  q <- (1-p)^(k-1)*p
+  return(q)
+}
+
+#Displaced poisson
+pois_dist <- function(lambda,k){
+  p <- (lambda^k) * exp(-lambda) / (factorial(k) * (1 - exp(-lambda)))
+}
+
+#Zeta truncated
+zetatrunc_dist <- function(k_max,gamma,k){
+  k^(-gamma)/(sum((1:k_max)^(-gamma)))
+}
+
+# Function to create the plots
+full_plot <- function(i,x,label,n.samples,deg_spec,df){
+  degree <- as.numeric(names(deg_spec))  # X-axis
+  vertices <- as.numeric(deg_spec)  # Y-axis
+  plot(degree, vertices, type = "l", main = label, lwd = 2,
+       xlab = "Degree", ylab = "Number of vertices", log = "xy") #plot of real data
+  
+  geo_prob <- sapply(x, geo_dist,p=df$p[i])
+  lines(x,geo_prob*n.samples,col = "blue",lwd = 2) #plot of geometric distribution
+  
+  pois_prob <- sapply(x, pois_dist,lambda=df$lambda[i])
+  lines(x,pois_prob*n.samples,col = "green",lwd = 2) #plot of Poisson distribution
+  
+  zeta_prob <- sapply(x, dzeta, shape = df$`gamma 1`[i])
+  lines(x,zeta_prob*n.samples,col = "orange",lwd = 2) #plot of Zeta distribution
+  
+  zeta2_prob <- sapply(x, dzeta, shape = 2)
+  lines(x,zeta2_prob*n.samples,col = "magenta",lwd = 2) #plot of Zeta (gamma = 2) distribution
+  
+  zetatrunc_prob <- sapply(x, zetatrunc_dist, gamma = df$`gamma 2`[i], 
+                           k_max = df$`k max`[i]) 
+  lines(x,zetatrunc_prob*n.samples,col = "red",lwd = 2) #plot of right-truncated Zeta distribution
+  
+  legend("topright", legend = c("Data","Geometric", "Poisson", "Zeta", 
+                                "Zeta (Lambda=2)", "Zeta truncated","Confidence interval"), 
+         col = c("black","blue","green","orange","magenta","red","red"), 
+         lty = c(1,1,1,1,1,1,2), lwd = 2)
+}
+
+# Code for language networks
+# Import the data and create the initial summary table
+source = read.table("list_in.txt", 
+                    header = TRUE,               # this is to indicate the first line of the file contains the names of the columns instead of the real data
+                    as.is = c("language","file") # this is need to have the cells treated as real strings and not as categorial data.
+)
+
+lang.df <- data.frame() #Summary table with all languages and important values
+
+for (x in 1:nrow(source)) {
+  lang.df <- write_table(source$language[x], source$file[x],lang.df)
+}
+
+colnames(lang.df) <- c("Language", "N", "Maximum degree", "M/N", "N/M")
+lang.df 
+print(xtable(lang.df), file = "Table languages.tex")
+
+
+# Tables with estimated parameters and AIC
 param.df <- data.frame() #table with best parameters
 se.df <- data.frame() #table with the standard errors
 AIC.df <- data.frame() #AIC table
@@ -119,58 +173,14 @@ for (i in 1:nrow(source)){
 colnames(param.df) <- c("Language", "lambda", "p", "gamma 1","gamma 2","k max")
 colnames(se.df) <- c("Language", "1", "2", "3", "5")
 colnames(AIC.df) <- c("Language", "1", "2", "3", "4", "5")
-
 param.df
 se.df
 AIC.df
 print(xtable(param.df), file = "Table Parameters.tex")
 print(xtable(AIC.df), file = "Table AIC.tex")
 
-# PLOTS
-# Probability functions
-#Displaced geometric
-geo_dist <- function(p,k){
-  q <- (1-p)^(k-1)*p
-  return(q)
-}
 
-#Displaced poisson
-pois_dist <- function(lambda,k){
-  p <- (lambda^k) * exp(-lambda) / (factorial(k) * (1 - exp(-lambda)))
-}
-
-#Zeta truncated
-zetatrunc_dist <- function(k_max,gamma,k){
-  k^(-gamma)/(sum((1:k_max)^(-gamma)))
-}
-
-full_plot <- function(i,x,label,n.samples,deg_spec,df){
-  degree <- as.numeric(names(deg_spec))  # X-axis
-  vertices <- as.numeric(deg_spec)  # Y-axis
-  plot(degree, vertices, type = "l", main = label, lwd = 2,
-       xlab = "Degree", ylab = "Number of vertices", log = "xy")
-  
-  geo_prob <- sapply(x, geo_dist,p=df$p[i])
-  lines(x,geo_prob*n.samples,col = "blue",lwd = 2)
-  
-  pois_prob <- sapply(x, pois_dist,lambda=df$lambda[i])
-  lines(x,pois_prob*n.samples,col = "green",lwd = 2)
-  
-  zeta_prob <- sapply(x, dzeta, shape = df$`gamma 1`[i])
-  lines(x,zeta_prob*n.samples,col = "orange",lwd = 2)
-  
-  zeta2_prob <- sapply(x, dzeta, shape = 2)
-  lines(x,zeta2_prob*n.samples,col = "magenta",lwd = 2)
-  
-  zetatrunc_prob <- sapply(x, zetatrunc_dist, gamma = df$`gamma 2`[i], 
-                           k_max = df$`k max`[i])
-  lines(x,zetatrunc_prob*n.samples,col = "red",lwd = 2)
-  legend("topright", legend = c("Data","Geometric", "Poisson", "Zeta", 
-                                "Zeta (Lambda=2)", "Zeta truncated","Confidence interval"), 
-         col = c("black","blue","green","orange","magenta","red","red"), 
-         lty = c(1,1,1,1,1,1,2), lwd = 2)
-}
-
+# Plot
 for (i in 1:nrow(source)){
   degree_sequence = read.table(source$file[i], header = FALSE)
   degree_spectrum = table(degree_sequence) 
@@ -187,13 +197,13 @@ for (i in 1:nrow(source)){
   
 }
 
-# SAMPLES FROM DISCRETE DISTRIBUTIONS
+# Samples from discrete distributions
+# Import the data and create the initial summary table
 folder_path <- "./samples_from_discrete_distributions/data"
 files <- list.files(path = folder_path, full.names = TRUE)
 prob_list <- c("geo 0.05","geo 0.1","geo 0.2","geo 0.4","geo 0.8","zeta 1.5","zeta 2.5",
           "zeta 2","zeta 3.5","zeta 3")
 
-# INTRODUCTION
 prob.df <- data.frame()
 
 for (x in 1:length(files)) {
